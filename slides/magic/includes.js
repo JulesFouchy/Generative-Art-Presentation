fa0202d540c8a778dc7e0cb9aae128b2c7a572e1
Jules Fouchy <jules.fouchy@ntymail.com>
2022-04-15 22:56:17 +0200

    Commit

--- slides/magic/includes.js
+++ slides/magic/includes.js
@@ -1,6 +1,7 @@
 document.write(`
 <script src="./magic/navigation.js"></script>
 <link rel="stylesheet" href="../drawings/magic/style.css" />
+<link rel="stylesheet" href="./magic/style.css" />
 <canvas></canvas>
 <script src="../drawings/magic/drawing_lib.js"></script>
 `)
